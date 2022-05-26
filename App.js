9f269599e7bc967b75313ea760ea1a9c2402a818
aligol20 <aligol20@gmail.com>
2022-05-26 15:22:03 +0430

    log

--- App.js
+++ App.js
@@ -20,9 +20,10 @@ import { GlobalOptionsContext, OptionsContext, getOptions } from "@options"
 const getNavigation = (modules, screens, initialRoute) => {
   const Navigation = () => {
     const routes = modules.concat(screens).map(mod => {
-      const pakage = mod.package;
-      const name = mod.value.title;
-      const Navigator = mod.value.navigator;
+      const pakage = mod.package
+      const name = mod.value.title
+      const Navigator = mod.value.navigator
+      console.log("*****")
       const Component = () => {
         return (
           <OptionsContext.Provider value={getOptions(pakage)}>
@@ -33,7 +34,7 @@ const getNavigation = (modules, screens, initialRoute) => {
       return <Stack.Screen key={name} name={name} component={Component} />
     })
 
-    const screenOptions = { headerShown: true };
+    const screenOptions = { headerShown: true }
 
     return (
       <NavigationContainer>
@@ -49,7 +50,7 @@ const getNavigation = (modules, screens, initialRoute) => {
   return Navigation
 }
 
-const getStore = (globalState) => {
+const getStore = globalState => {
   const appReducer = createReducer(globalState, _ => {
     return globalState
   })
